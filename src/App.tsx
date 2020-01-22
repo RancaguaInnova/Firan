5fa1dc095e7afd2e9149f0abd8a81fc86201af54
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-01-22 17:01:45 -0300

    add menu  home

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
 import React, { Suspense } from 'react'
 import { Router, RouteComponentProps } from '@reach/router'
-
 import Header from 'components/Header'
 import Loading from 'components/Loading'
-
-import './App.css'
+import * as Sentry from '@sentry/browser'
+import Menu from 'components/Menu'
 
 const Home = React.lazy(() => import('pages/Home'))
-const Events = React.lazy(() => import('pages/Events'))
+const Register = React.lazy(() => import('pages/Register'))
 const News = React.lazy(() => import('pages/News'))
 const Services = React.lazy(() => import('pages/Services'))
 
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://66358d8509bd4e13bc78e9f6b63e4eb5@sentry.io/1383062',
+    debug: true
+  })
+  console.log('Sentry loaded!')
+}
+
 const App: React.FC = () => {
-	return (
-		<div className='App'>
-			<Header />
-			<Suspense fallback={<Loading />}>
-				<Router>
-					<RouterPage path='/' pageComponent={<Home />} default />
-					<RouterPage path='/eventos' pageComponent={<Events />} />
-					<RouterPage path='/noticias' pageComponent={<News />} />
-					<RouterPage path='/servicios' pageComponent={<Services />} />
-				</Router>
-			</Suspense>
-		</div>
-	)
+  return (
+    <div className='App'>
+      <Header />
+      <Suspense fallback={<Loading />}>
+        <Router>
+          <RouterPage path='/' pageComponent={<Home />} default />
+          <RouterPage path='/registro' pageComponent={<Register />} />
+          <RouterPage path='/noticias' pageComponent={<News />} />
+          <RouterPage path='/servicios' pageComponent={<Services />} />
+        </Router>
+      </Suspense>
+      <Menu />
+    </div>
+  )
 }
 
 export default App
 
-const RouterPage = (props: { pageComponent: JSX.Element } & RouteComponentProps) => props.pageComponent
+const RouterPage = (props: { pageComponent: JSX.Element } & RouteComponentProps) =>
+  props.pageComponent
---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.css'
+
+const Register: React.FC = () => {
+  return <div className='Register'>Hola soy la pagina de registro</div>
+}
+
+export default Register
