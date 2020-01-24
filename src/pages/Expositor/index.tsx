160faa1134751a2ba0556dd2c2e2d4f1db9e199b
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-01-24 13:26:06 -0300

    up
    up information and expositor section

--- src/pages/Expositor/index.tsx
+++ src/pages/Expositor/index.tsx
@@ -1,8 +1,60 @@
 import React from 'react'
 import './styles.css'
-
+import { Menu } from 'evergreen-ui'
+import { Link } from '@reach/router'
+import { FiUsers } from 'react-icons/fi'
+import { Accordion, AccordionTab } from 'primereact/accordion'
+import 'primereact/resources/themes/nova-light/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 const Expositor: React.FC = () => {
-  return <div className='Expositor'>Hola soy la pagina de expositores</div>
+  return (
+    <div className='content'>
+      <Menu>
+        <Link to='/'>
+          <Menu.Item className='menuItems'>
+            <FiUsers className='menuIcons' />
+            <span className='menuItem'>EXPOSITORES</span>
+          </Menu.Item>
+        </Link>
+      </Menu>
+      <Accordion>
+        <AccordionTab header='Por qué Exponer?' contentClassName='queesfiranHeader'>
+          <h4 className='TitleExpositor'>Por qué Exponer?</h4>
+          <div className='ContentExpositor'>
+            Es la instancia en donde convergen las distintas áreas economicas y sociales de la
+            región, en un espacio multisectorial potenciando sus capacidades de desarrollo y
+            poroductos de alto nivel, oportunidad de presentar las novedades al mercado nacional e
+            internaciona. Esta versión apuesta por la innovación, la virtud a la reutilización y a
+            pontenciar las capacidades de la comunidad.
+          </div>
+          <div className='ExpositorCenter'>
+            "Generar nuevas oportunidades de negocios y vínculos comerciales"
+          </div>
+        </AccordionTab>
+        <AccordionTab header='Quienes exponen?' contentClassName='queesfiranHeader'>
+          <h4 className='TitleExpositor'>Quienes exponen?</h4>
+          <div className='ContentExpositor'></div>
+        </AccordionTab>
+        <AccordionTab header='Valores Stands' contentClassName='queesfiranHeader'>
+          <h4 className='TitleExpositor'>Valores Stands</h4>
+          <div className='ContentExpositor'></div>
+        </AccordionTab>
+        <AccordionTab header='Valores Stands' contentClassName='queesfiranHeader'>
+          <h4 className='TitleExpositor'>Plano Ferial</h4>
+          <div className='ContentExpositor'></div>
+        </AccordionTab>
+        <AccordionTab header='Valores Stands' contentClassName='queesfiranHeader'>
+          <h4 className='TitleExpositor'>Brochure</h4>
+          <div className='ContentExpositor'></div>
+        </AccordionTab>
+        <AccordionTab header='Valores Stands' contentClassName='queesfiranHeader'>
+          <h4 className='TitleExpositor'>Manual del Expositor</h4>
+          <div className='ContentExpositor'></div>
+        </AccordionTab>
+      </Accordion>
+    </div>
+  )
 }
 
 export default Expositor
--- src/pages/Information/index.tsx
+++ src/pages/Information/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Menu } from 'evergreen-ui'
+import { Menu, Button } from 'evergreen-ui'
 import { Link } from '@reach/router'
 import { FaInfoCircle } from 'react-icons/fa'
 import { Accordion, AccordionTab } from 'primereact/accordion'
@@ -10,7 +10,7 @@ import './styles.css'
 
 const Events: React.FC = () => {
   return (
-    <div className='Events'>
+    <div className='content'>
       <Menu>
         <Link to='/'>
           <Menu.Item className='menuItems'>
@@ -20,23 +20,75 @@ const Events: React.FC = () => {
         </Link>
       </Menu>
       <Accordion>
-        <AccordionTab header='¿Que es firan?' contentClassName='queesfiranHeader'>
-          ¿Que es firan?
+        <AccordionTab header='¿Que es FIRAN?' contentClassName='queesfiranHeader'>
+          <h4 className='TitleInformation'>
+            ¿Que es <b>firan?</b>
+          </h4>
+          <div className='ContentInformation'>
+            Feria Internacional Rancagua 20220, tiene por objetivo mostrar todas las oportunidades y
+            capacidades que ofrece la ciudad de Rancagua a sus habitantes y al mundo. Durante dos
+            días de feria , se realizarán diversas actividades de tipo cultural y
+            empresarial,charlas,exposiciones, intervenciones artísticas y más.
+          </div>
+          <div className='FooterInformation'>Te invitamos a ser protagonistas</div>
+          <div className='FooterInformation2'>de FIRAN 2020</div>
         </AccordionTab>
         <AccordionTab header='Ubicación' headerClassName='ubicacionHeader'>
-          Ubicación
+          <h4 className='TitleInformation'>Ubicación</h4>
+          <div className='ContentInformation'>
+            <ul>
+              <li className='displayDireccion'>Medialuna Monumental de Rancagua</li>
+              <li className='displayDireccion'>
+                <div className='direccion'>
+                  Av .España con German Ibara, Rancagua Región del Libertador Gral. Bernardo
+                  O'higgins
+                </div>
+                <div className='direccion'>
+                  <Button marginRight={16} className='buttonOpenMaps'>
+                    Ver rutas en google maps
+                  </Button>
+                </div>
+              </li>
+            </ul>
+          </div>
         </AccordionTab>
         <AccordionTab header='Fechas y Horarios' headerClassName='fechasYhorariosHeader'>
-          Fechas y Horarios
+          <h4 className='TitleInformation'>Fechas y Horarios</h4>
+          <div className='ContentInformation'>
+            <ul className='gridDisplay'>
+              <li className='displayDireccion'>Viernes 7 - Sábado 8 de Noviembre 2020</li>
+              <li className='displayDireccion'>Horario de 11:00 a 20:00 hrs</li>
+              <li className='displayDireccion'>Evento apto para familia y todo público</li>
+
+              <li className='displayDireccion'>
+                <div className='direccion'>11:00 a 20:00 hrs</div>
+                <div className='direccion'>imagenes de calendario</div>
+              </li>
+            </ul>
+          </div>
         </AccordionTab>
         <AccordionTab header='Entradas y Valores' headerClassName='entradasYvaloresHeader'>
-          Entradas y Valores
+          <h4 className='TitleInformation'>Entradas y Valores</h4>
+          <div className='ContentInformation'>
+            Para aceder a entradas (e-tickets) debera descargar la APP RANCAGUA DIGITAL, y seguir
+            los pasos para obtner tus tickets.
+          </div>
+          <div className='ContentInformation'>
+            <ul className='gridDisplay'>
+              <li className='displayDireccion'>Gratuita</li>
+              <li className='displayDireccion'>App Rancagua Digital</li>
+              <li className='displayDireccion'>Público General</li>
+              <li className='displayDireccion'>Máximo 2 tickets por App</li>
+            </ul>
+          </div>
         </AccordionTab>
         <AccordionTab header='Plano Ferial' headerClassName='planoFerialHeader'>
-          Plano Ferial
+          <h4 className='TitleInformation'>Plano Ferial</h4>
+          <div className='ContentInformation'></div>
         </AccordionTab>
         <AccordionTab header='Descarga Volante Ferial' headerClassName='descargaVolanteFerial'>
-          Descarga volante ferial
+          <h4 className='TitleInformation'>Descarga volante ferial</h4>
+          <div className='ContentInformation'></div>
         </AccordionTab>
       </Accordion>
     </div>
