d6d308f237e6c4e25113ef18a94c4200fb6116ca
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-01-24 08:51:17 -0300

    #2 issue

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './styles.css'
 
 const Home: React.FC = () => {
-  return <div className='Home'></div>
+  return <div className='Home'>home</div>
 }
 
 export default Home
